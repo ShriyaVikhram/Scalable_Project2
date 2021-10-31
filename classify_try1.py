be511e84fe715e80d7279b72b322433d8ed29e07
ShriyaVikhram <93049249+ShriyaVikhram@users.noreply.github.com>
2021-10-31 14:57:49 +0000

    Update classify_try1.py

--- classify_try1.py
+++ classify_try1.py
@@ -28,29 +28,29 @@ symbols_file.close()
 
 print("Classifying captchas with symbol set {" + captcha_symbols + "}")
 
-with tf.device('/cpu:0'):
-    with open(output, 'w') as output_file:
-        model = tf.lite.Interpreter(model_path="converted_model.tflite")
-        model.allocate_tensors()
-        input_details = model.get_input_details()
-        output_details = model.get_output_details()
 
-        for x in os.listdir(captcha_dir):
-            # load image and preprocess it
-            raw_data = cv2.imread(os.path.join(captcha_dir, x))
-            rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
-            image = numpy.array(rgb_data, dtype=numpy.float32) / 255.0
-            (c, h, w) = image.shape
-            image = image.reshape([-1, c, h, w])
-            model.set_tensor(input_details[0]['index'], image)
-            model.invoke()
-            output_data = []
-            for i in range(6):
-                output_data.append(model.get_tensor(output_details[i]['index']))
-            output_data = numpy.array(output_data, dtype=numpy.float32)
-            output_file.write(x + ", " + decode(captcha_symbols, output_data) + "\n")
+with open(output, 'w') as output_file:
+    model = tf.lite.Interpreter(model_path="converted_model.tflite")
+    model.allocate_tensors()
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
 
-            print('Classified ' + x)
+    for x in os.listdir(captcha_dir):
+        # load image and preprocess it
+        raw_data = cv2.imread(os.path.join(captcha_dir, x))
+        rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
+        image = numpy.array(rgb_data, dtype=numpy.float32) / 255.0
+        (c, h, w) = image.shape
+        image = image.reshape([-1, c, h, w])
+        model.set_tensor(input_details[0]['index'], image)
+        model.invoke()
+        output_data = []
+        for i in range(6):
+            output_data.append(model.get_tensor(output_details[i]['index']))
+        output_data = numpy.array(output_data, dtype=numpy.float32)
+        output_file.write(x + ", " + decode(captcha_symbols, output_data) + "\n")
+
+        print('Classified ' + x)
 
 # if __name__ == '__main__':
 #     main()
