e56f40ae7393d2f921193f0ee866a2045412232d
ShriyaVikhram <shriya@DESKTOP-BH0E8TS.localdomain>
2021-11-12 11:42:00 +0000

    updating get_captcha

--- get_captchas.py
+++ get_captchas.py
@@ -1,7 +1,7 @@
 import csv
 import urllib.request
 
-file = open('vikhrams-challenge-filenames.csv', "rU")
+file = open('index.php\?download\=noresume_speed\&shortname\=bonus', "rU")
 filelist = csv.reader(file, delimiter=',')
 for rec in filelist:
-	   urllib.request.urlretrieve("https://cs7ns1.scss.tcd.ie/index.php?download=noresume_speed&shortname=vikhrams&myfilename="+rec[0], rec[0])
+	   urllib.request.urlretrieve("https://cs7ns1.scss.tcd.ie/index.php?download=noresume_speed&shortname=bonus&myfilename="+rec[0], rec[0])
